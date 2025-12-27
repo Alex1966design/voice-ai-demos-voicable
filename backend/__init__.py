f00150dd7766ebd4122729005c160bf1c0b189e3
Alex Zerocoder <a.kamenetsky@mail.ru>
2025-12-27 10:04:35 +0300

    fix: make backend a package for Railway import

--- backend/__init__.py
+++
@@ -1,6 +0,0 @@
-# assistant/__init__.py
-
-from . import stt_client, llm_client, tts_client
-from .alina import AlinaAssistant
-
-__all__ = ["stt_client", "llm_client", "tts_client", "AlinaAssistant"]
